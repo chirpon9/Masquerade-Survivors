3f31d0a01642a754a6f44008d30c8ac28cc8d820
Donald-cmd-ops <donaldwatchesracing@gmail.com>
2026-01-31 14:38:32 +0800

    added bullet

---
+++ bullet_reworked.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+@export var speed = 125.0
+
+func _ready():
+	add_to_group("mobs")
+	
+	var player = get_tree().get_first_node_in_group("player")
+	if player:
+		var direction = (player.global_position - global_position).normalized()
+		look_at(player.global_position)
+		# rotation is used for rectangular projectiles
+		rotation += deg_to_rad(90)
+		velocity = direction * speed
+
+func _physics_process(_delta):
+	move_and_slide()
+
+	
